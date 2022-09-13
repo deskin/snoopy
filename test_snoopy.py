126a8c8871e4f66ac876a1719cdb91202db8f64e
Deskin Miller <deskinm@umich.edu>
2022-09-13 14:00:35 -0700

    Add pytest

---
+++ test_snoopy.py
@@ -0,0 +1,10 @@
+import pytest
+from math import isclose
+
+import orbital as orb
+import orbital_snoopy as sn
+
+class TestSnoopy:
+		def test_earth_orbit_2017(self):
+				o = sn.earth_orbit_2017()
+				assert isclose(150855105915.9792, orb.utilities.norm(o.r))
